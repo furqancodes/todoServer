276a5bf7e986a8cab0ffa1f07c0b2345e4f52ffe
DESKTOP-SH6NK5D\Administrator1 <furqan_ashraf@outlook.com>
2021-09-03 11:32:16 +0500

    delete tasks

--- routers/routes.js
+++ routers/routes.js
@@ -1,30 +1,32 @@
-
-const express = require('express');
-const tasks = require("../models/tasks")
+const express = require("express");
+const tasks = require("../models/tasks");
 const router = express.Router();
 
-router.get('/show', function(req, res){
-   res.send(tasks);
+router.get("/show", async (req, res) => {
+  const allTasks = await tasks.find({});
+  res.send(allTasks);
 });
-// router.post('/add', async(req, res)=>{
-//     // console.log(req.body)
-//     const addTasks = new tasks({...req.body})
-//     console.log(addTasks)
-//     try {
-//         await addTasks.save()
-//         res.status(201).send(addTasks)
-//     } catch (error) {
-//         res.status(400).send(error)
-//     }
-//    res.send('POST route on things.');
-// });
+
 router.post("/add", async (req, res) => {
-    const newTask = new tasks(req.body);
-    try {
-      await newTask.save();
-      res.status(201).send({ newTask });
-    } catch (err) {
-      res.status(400).send(err.message);
+  const newTask = new tasks(req.body);
+  try {
+    await newTask.save();
+    res.status(201).send({ newTask });
+  } catch (err) {
+    res.status(400).send(err.message);
+  }
+});
+
+router.delete("/tasks/:taskid", async (req, res) => {
+  console.log(req.params.taskid)
+  try {
+    const taskDelete = await tasks.findByIdAndDelete(req.params.taskid);
+    if (!taskDelete) {
+      res.status(404).send("not found");
     }
-  });
+    res.send(taskDelete);
+  } catch (taskDeleteError) {
+    res.status(500).send(taskDeleteError);
+  }
+});
 module.exports = router;
