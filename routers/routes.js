240cf356b624ed3514f499114ac2ca54c0cd16a2
DESKTOP-SH6NK5D\Administrator1 <furqan_ashraf@outlook.com>
2021-09-08 15:15:56 +0500

    post slack integretad

--- routers/routes.js
+++ routers/routes.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const axios = require("axios")
 const tasks = require("../models/tasks");
 const router = express.Router();
 
@@ -9,10 +10,13 @@ router.get("/show", async (req, res) => {
 
 router.post("/add", async (req, res) => {
   const newTask = new tasks(req.body);
+  
   try {
     await newTask.save();
-    const allTasks = await tasks.find({});
-    res.status(201).send(allTasks);
+    await axios.post("https://hooks.slack.com/services/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",{
+      text:`Task added : ${newTask.description}`,
+    })
+    res.status(201).send({message:"task added"});
   } catch (err) {
     res.status(400).send(err.message);
   }
